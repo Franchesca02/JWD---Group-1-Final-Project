0b3c4cdb9764fb74a0c805be94264b149b0166f2
Noah Macaluso <noahm714@gmail.com>
2022-11-09 20:02:40 -0600

    added functionality to submit button (form field data)

--- index.js
+++ index.js
@@ -1,40 +1,34 @@
+function validFormFieldInput(data){
 
-  const inputName = document.getElementById('name');
-  const Name = inputName.value;
-
-  const inputDescription = document.getElementById('description');
-  const description = inputDescription.value;
-
-  const inputAssignedTo = document.getElementbyId('assignedTo');
-  const assignedTo = inputAssignedTo.value;
-
-  const inputDate = document.getElementById('dueDate');
-  const dueDate = inputDate.value;
-
-  const taskButton = document.getElementById('submitButton');
-
-  const success = document.getElementById('success');
-  const danger = document.getElementById('danger');
-
- 
-
-  function validFormFieldInput(data){
-
-    if (data.value === ''){
-        success.hidden = true;
-        danger.hidden = false;
-        danger.innerHTML = 'Empty Fields!';
-        
+  if (typeof data !== 'string'){
+        emptyField.style.visibility = 'visible';
+      //alert('Fields Cannot Be Empty!');
+      
   } else {
-    success.hidden = false;
-    danger.hidden = true;
-    success.innerHTML = 'Submitted!';
+    success.style.visibility = 'visible';
+    
+    //alert('Submitted!');
   }
-}
+};
 
-taskButton.onclick = () => {
-    validFormFieldInput(Name);
+newTaskForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+const name = document.getElementById('name');
+const description = document.getElementById('description');
+const assignedTo = document.getElementById('assignedTo');
+const dueDate = document.getElementById('dueDate');
+
+const submitButton = document.getElementById('submitButton');
+
+const success = document.getElementById('success');
+const emptyField = document.getElementById('emptyField');
+
+const newTaskForm = document.getElementById('newTaskForm');
+
+    validFormFieldInput(name);
     validFormFieldInput(description);
     validFormFieldInput(assignedTo);
     validFormFieldInput(dueDate);
-}
+});
+    
